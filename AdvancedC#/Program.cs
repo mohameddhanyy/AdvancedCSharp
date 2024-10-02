c37a2b42062713f89ff758f4e0d0f9d72c424400
Mohamed Hani <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-10-02 23:11:09 +0300

    Generic and Delegate Types

--- AdvancedC#/Program.cs
+++ AdvancedC#/Program.cs
@@ -4,63 +4,117 @@
     {
         static void Main(string[] args)
         {
-            Any<int> a = new Any<int>();
-            a.Add(1);
-            a.Add(2);
-            a.Add(3);
-            Console.WriteLine($"check is Empty => {a.IsEmpty} ,, Count is {a.Count}");
-            a.RemoveAt(1);
+            #region Generic
+            var p = new Person("Mohamed", "Hany");
+            var s = new Person("MAY", "HANY");
+
+            var a = new Any<Person>();
+            a.Add(p);
+            a.Add(s);
             a.Display();
 
+            //a.Add(1);
+            //a.Add(2);
+            //a.Add(3);
+            //Console.WriteLine($"check is Empty => {a.IsEmpty} ,, Count is {a.Count}");
+            //a.RemoveAt(1);
+            //a.Display();
+
+            #endregion
+
+            #region Generic Delegate Type
+            IEnumerable<int> l = new int[] { 1, 2, 5, 3, 4, 6, 7, 8, 9, 10 };
+            IEnumerable<decimal> d = new decimal[] { 1.22m, 3.55m ,5.55m, 3.99m, 4.99m, 6.44m, 7.66m, 8.22m, 9.11m, 10.00m };
+
+            PrintNumber(l, x=>x>6 , ()=> Console.WriteLine("numbers greater than 6"));
+
+            PrintNumber(l, x => x > 7, () => Console.WriteLine("numbers greater than 7"));
+
+            PrintNumber(l, x => x % 2 == 0, () => Console.WriteLine("Is Even"));
+
+
+
+            #endregion
+        }
+
+        static void PrintNumber<T>(IEnumerable<T> nums, Predicate<T> predicate,Action a)
+        {
+            a();
+            foreach (T n in nums)
+            {
+                if (predicate(n))
+                {
+                    Console.WriteLine(n);
+                }
+            }
+        }
+
+        #region Generic
+        class Person
+        {
+            private string fname;
+            private string lname;
+
+            public Person(string fname, string lname)
+            {
+                this.fname = fname;
+                this.lname = lname;
+            }
+
+            public override string ToString()
+            {
+                return $"'{fname} {lname}'";
+            }
+        }
+        class Any<T> where T : class
+        {
+            private T[] _items;
+            public void Add(T item)
+            {
+                if (_items == null)
+                    _items = new T[] { item };
+                else
+                {
+                    var length = _items.Length;
+                    var dest = new T[length + 1];
+                    for (int i = 0; i < length; i++)
+                        dest[i] = _items[i];
+                    dest[dest.Length - 1] = item;
+                    _items = dest;
+                }
+            }
+            public void RemoveAt(int position)
+            {
+                if (_items == null)
+                    return;
+                var length = _items.Length;
+                var dest = new T[length - 1];
+                var index = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    if (i == position)
+                        continue;
+                    dest[index++] = _items[i];
+                }
+                _items = dest;
+            }
+
+            public bool IsEmpty => _items.Length == 0 || _items is null;
+            public int Count => _items.Length;
+
+            public void Display()
+            {
+                Console.Write("[");
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    Console.Write(_items[i]);
+                    if (i < _items.Length - 1)
+                        Console.Write(", ");
+                }
+                Console.WriteLine("]");
+            }
         }
     }
 }
+#endregion
 
-class Any<T>
-{
-    private T[] _items;
-    public void Add(T item)
-    {
-        if (_items == null)
-            _items = new T[] { item };
-        else
-        {
-            var length = _items.Length;
-            var dest = new T[length + 1];
-            for (int i = 0; i < length; i++)
-                dest[i] = _items[i];
-            dest[dest.Length-1] = item;
-            _items = dest;
-        }
-    }
-    public void RemoveAt(int position)
-    {
-        if (_items == null)
-            return;
-        var length = _items.Length;
-        var dest = new T[length-1];
-        var index = 0;
-        for (int i = 0; i < length; i++)
-        {
-            if (i == position)
-                continue;
-            dest[index++] = _items[i];
-        }
-        _items = dest;
-    }
-
-    public bool IsEmpty=> _items.Length == 0 || _items is null;
-    public int Count => _items.Length;
-
-    public void Display()
-    {
-        Console.Write("[");
-        for (int i = 0; i < _items.Length; i++)
-        {
-            Console.Write(_items[i]);
-            if(i < _items.Length - 1)
-                Console.Write(", ");
-        }
-        Console.WriteLine("]");
-    }
-}
